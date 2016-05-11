464e0046cdee8694828d6b0594ed769b35526938
Matthew Ward <iammatthewward@gmail.com>
2016-05-11 12:09:11 +0100

    Thermostat can decrease temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,6 +6,19 @@ describe('Thermostat', function(){
 
   it('has a default temperature of 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20);
-
   });
+
+  describe('Changing temperature', function(){
+    it('can increase the temperature with the up button', function(){
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(21);
+    });
+
+    it('can decrease the temperature with the down button', function(){
+      thermostat.decreaseTemperature();
+      expect(thermostat.temperature).toEqual(19);
+    });
+  });
+
+
 });
