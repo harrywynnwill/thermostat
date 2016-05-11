bddec01f59e63528b0853cf194fee6a9155d4db0
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-05-11 16:19:50 +0100

    power saving functionality added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -19,6 +19,33 @@ describe('Thermostat', function(){
       expect(thermostat.temperature).toEqual(19);
     });
   });
+  it('has a minimum temperature of 10 degrees', function(){
+      for(i=0; i <11; i++ ){
+        thermostat.decreaseTemperature();
+      }
+      expect(thermostat.temperature).toEqual(10);
+  });
+  it('has a maximum temperature of 32 degrees', function(){
+      thermostat.powerSavingOff();
+      for(i=0; i <13; i++ ){
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.temperature).toEqual(32);
+  });
+  it('has power saving on by default', function(){
+    expect(thermostat.powerSaving).toBe(true);
+  });
+  it('responds to #powerSavingOff', function(){
+    thermostat.powerSavingOff();
+    expect(thermostat.powerSaving).toBe(false)
+  });
+  it('has a maximum of 25 when power saving is on', function(){
+    thermostat.powerSavingOn;
+    for(i=0; i <6; i++ ){
+      thermostat.increaseTemperature();
+    }
+    expect(thermostat.temperature).toEqual(25);
 
+  });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,31 @@
 function Thermostat(){
 
   this.temperature = 20;
+  this.minimumTemp = 10;
+  this.maximumTemp = 25;
+  this.powerSaving = true;
 
   Thermostat.prototype.increaseTemperature = function() {
+
+    if(this.temperature < this.maximumTemp) {
     this.temperature += 1;
+    }
   };
 
   Thermostat.prototype.decreaseTemperature = function() {
-    this.temperature -= 1;
+    if(this.temperature > this.minimumTemp){
+        this.temperature -= 1;
+    }
+  };
+  Thermostat.prototype.isPowerSaving = function () {
+    return this.powerSaving;
   };
 }
+Thermostat.prototype.powerSavingOff = function () {
+  this.powerSaving = false;
+  this.maximumTemp = 32;
+};
+Thermostat.prototype.powerSavingOn = function () {
+  this.powerSaving = true;
+  this.maximumTemp = 25;
+};
